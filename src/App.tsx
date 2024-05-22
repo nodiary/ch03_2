163e54ac16761fb7e8f78a1a18575aac9a5660aa
roh <rohilkie94@gmail.com>
2024-05-22 16:08:23 +0900

    ch03-2 - 테일윈드 css 리액트 프로젝트 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Color from './pages/Color'
+import Tailwindcss from './pages/Tailwindcss'
+import TextsTest from './pages/TextsTest'
+import './App.css'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Color />
+      <Tailwindcss />
+      <TextsTest />
     </div>
-  );
+  )
 }
-
-export default App;
